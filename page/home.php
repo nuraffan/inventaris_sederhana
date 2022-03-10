9327b104ba1ccba66e9ba71ddd9aeb41822eaa0d
nuraffan <muh.nuraffan@gmail.com>
2022-03-10 12:34:33 +0700

    Add files via upload

---
+++ page/home.php
@@ -0,0 +1,6 @@
+<div class="jumbotron">
+        <h1>Selamat Datang, <?= $user ?>! </h1>
+        <p>Selamat datang di aplikasi inventaris Kantor</p>
+        <p>Aplikasi ini merupakan aplikasi untuk uji level kelas XII RPL</p>
+        
+      </div>
